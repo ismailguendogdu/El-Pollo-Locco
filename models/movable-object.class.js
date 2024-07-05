66b6eceed4b1dfc644c0504c67e7be53da0cf7ba
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-07-05 10:46:24 +0200

    added character and chicken

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,8 +1,18 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 250;
     img;
+    height = 150;
+    width = 100;
+    
 
+
+
+    //laodImage('img/test.png');
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id = "image">
+        this.img.src = path;
+    }
 
      moveRight() {
         console.log('Move right');
