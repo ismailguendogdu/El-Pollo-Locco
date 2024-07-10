f38a1835e057e1b04e14792ccad623335f5d95f0
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-07-10 10:31:01 +0200

    added hurt, dead  colliding function

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,6 @@
 class Character extends MovableObject {
 
-    height = 280;
+    height = 250;
     y = 80;
     speed = 10;
     IMAGES_WALKING = [
@@ -9,15 +9,48 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-23.png',
         'img/2_character_pepe/2_walk/W-24.png',
         'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png',
+        'img/2_character_pepe/2_walk/W-26.png'
     ];
+
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/2_character_pepe/5_dead/D-51.png',
+        'img/2_character_pepe/5_dead/D-52.png',
+        'img/2_character_pepe/5_dead/D-53.png',
+        'img/2_character_pepe/5_dead/D-54.png',
+        'img/2_character_pepe/5_dead/D-55.png',
+        'img/2_character_pepe/5_dead/D-56.png',
+        'img/2_character_pepe/5_dead/D-57.png'
+    ];
+
+    IMAGES_HURT = [
+        'img/2_character_pepe/4_hurt/H-41.png',
+        'img/2_character_pepe/4_hurt/H-42.png',
+        'img/2_character_pepe/4_hurt/H-43.png'
+    ];
+
     world;
     walking_sound = new Audio('audio/walking.mp3');
+    jumping_sound = new Audio('audio/jump.mp3')
    
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.applyGravity();
         this.animate();
     }
@@ -27,14 +60,19 @@ class Character extends MovableObject {
         setInterval(() => {
             this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
-                this.otherDirection = false;
-                this.walking_sound.play();
+               this.moveRight();
+               this.otherDirection = false;
+               this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
-                this.x -= this.speed;
+                this.moveLeft();
                 this.otherDirection = true;
                 this.walking_sound.play();
+            }
+
+            if(this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
+                this.jumping_sound.play();
             }
             
             this.world.camera_x = -this.x + 100;
@@ -44,17 +82,22 @@ class Character extends MovableObject {
 
         setInterval(() => {
 
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                
-                //Walk animation
-              
-              this.playAnimation(this.IMAGES_WALKING);
-            }
+            if(this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else if(this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if(this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
+            }    
         }, 50); 
     }
        
 
     jump() {
-
+       this.speedY = 30;
     }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -10,21 +10,67 @@ class MovableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    enery = 100;
+    lastHit = 0;
 
 
     applyGravity() {
         setInterval(() => {
-            if(this.y < 180) {
+            if(this.isAboveGround() || this.speedY > 0) {
             this.y -= this.speedY;
             this.speedY -= this.acceleration;
             }
         }, 1000 / 25);
     }
 
+    isAboveGround() {
+        return this.y < 180;
+    }
+
     //laodImage('img/test.png');
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id = "image">
         this.img.src = path;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        if(this instanceof Character || this instanceof Chicken) {
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+        }
+    }
+
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+        this.y + this.height > mo.y &&
+        this.x < mo.x &&
+        this.y < mo.y + mo.height; 
+    }
+
+    hit() {
+        this.enery -= 5;
+        if(this.enery < 0) {
+            this.enery = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit; // Difference in ms
+        timepassed = timepassed / 1000; // Difference in s
+        return timepassed < 1;
+    }
+
+    isDead() {
+        return this.enery == 0;
     }
 
     loadImages(arr) {
@@ -36,17 +82,35 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
-    moveLeft() {
-        setInterval(() => {
-        this.x -= this.speed;
-        },1000 / 60);
+    moveRight() {
+        this.x += this.speed;          
     }
+
+    moveLeft() {
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
+    }
+   
+    // if (character.x + character.width > chicken.x &&      
+    //     character.y + character.height > chicken.y &&   
+    //     character.x < chicken.x &&   
+    //     character.y < chicken.y + chicken.height   
+    // )
+
+    // isColliding (obj) {
+    //     return  (this.X + this.width) >= obj.X && this.X <= (obj.X + obj.width) && 
+    //             (this.Y + this.offsetY + this.height) >= obj.Y &&
+    //             (this.Y + this.offsetY) <= (obj.Y + obj.height) && 
+    //             obj.onCollisionCourse;
 
    
 }
